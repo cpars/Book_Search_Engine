8d47c392cdd9882f1967fe3a2cae3bd0872964e2
Corey P <cparsons0730@yahoo.com>
2025-03-17 20:59:34 -0500

    Added apollo server connection to server.ts

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,7 +1,11 @@
+import { ApolloServer } from '@apollo/server';
+import { expressMiddleware } from '@apollo/server/express4';
+import { typeDefs, resolvers } from './schemas/index.js'; 
+import { authenticateToken } from './services/auth.js';
 import express from 'express';
 import path from 'node:path';
 import db from './config/connection.js';
-import routes from './routes/index.js';
+import { fileURLToPath } from 'node:url'; 
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -9,13 +13,38 @@ const PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+async function startServer() {
+  await server.start();
+
+app.use('/graphql', expressMiddleware(server,
+  {
+    context: authenticateToken,
+  }
+));
+
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename)
+  
+  app.use(express.static(path.join(__dirname, '../client/dist')));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+  });
 }
 
-app.use(routes);
-
 db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+app.listen(PORT, () => {
+  console.log(`API server running on port ${PORT}!`);
+  console.log(`🚀 Use GraphQL at http://localhost:${PORT}/graphql`);
 });
+})
+};
+
+startServer();
