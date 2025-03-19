4bb665f95365bf9b4e1ce80cabfe38569d48758c
Corey P <cparsons0730@yahoo.com>
2025-03-18 19:47:02 -0500

    inserted _ for req in server.ts

--- server/src/server.ts
+++ server/src/server.ts
@@ -34,7 +34,7 @@ if (process.env.NODE_ENV === 'production') {
   
   app.use(express.static(path.join(__dirname, '../client/dist')));
 
-  app.get('*', (req, res) => {
+  app.get('*', (_req, res) => {
     res.sendFile(path.join(__dirname, '../client/dist/index.html'));
   });
 }
